526df8acdfc415bde557eb0e1b21c96c8bea9f01
Jonathan Arino <arinojonathan@gmail.com>
2018-03-17 23:31:40 +0800

    check if user logging in has email in database

--- server/account-creation.js
+++ server/account-creation.js
@@ -2,9 +2,30 @@ import { Accounts } from 'meteor/accounts-base';
 
 // supply data for extending the User Collection when user signs in for the first time
 
+// Accounts.validateNewUser((user) => {
+//     var email = user.services.google.email;
+
+//     console.log(email);
+
+//     if (!email) throw Meteor.Error(404, "User not found. Contact Admin for details.");
+
+    
+
+//     return true;
+
+// });
+
 Accounts.onCreateUser((options, user) => {
     if (!user.services.google && !user.username === 'admin') {
         throw new Error('Expected login with Google only');
+    }
+
+    console.log('went here');
+
+    let foundEmail = Meteor.users.find({ email: user.services.google.email });
+
+    if (!foundEmail.length) { 
+        throw new Error('Email not found');
     }
 
     user.profile = options.profile || {};
