a9c40fbec4d8075ce62ff73b3f589c6a35d56991
Jonathan Arino <arinojonathan@gmail.com>
2018-04-30 01:14:02 +0800

    ui improvements

--- imports/ui/components/userProfile/classPanel.jsx
+++ imports/ui/components/userProfile/classPanel.jsx
@@ -11,7 +11,8 @@ class ClassPanel extends Component {
         super(props);
         this.state = {
             teacher: props.teacher,
-            sections : []
+            sections: [],
+            activeSection: null,
         }
     }
 
@@ -24,33 +25,57 @@ class ClassPanel extends Component {
     render() {
         const { teacher } = this.state;
         return (
-            <Card fluid>
-                <Card.Content header='Classes' />
-                <Card.Content>
-                    <List divided verticalAlign='middle' selection animated>
-                        {this.props.sections.length != 0 ?
-                            this.state.sections.map((section) => 
-                                <List.Item key={section._id}>
-                                    <List.Content floated='right'>
-                                        <Button icon labelPosition='left'>
-                                            <Icon name='send outline' />
-                                            Message
-                                        </Button>
-                                    </List.Content>    
-                                    <List.Content floated='right' verticalAlign='middle'> 
-                                        <List.Header verticalAlign='middle'>{section.code}</List.Header>
-                                    </List.Content>
-                                    <List.Content verticalAlign='middle'>
-                                        <List.Header verticalAlign='middle'>{section.subject} - {section.sectionName}</List.Header>
-                                    </List.Content>
-                                </List.Item>
-                            ) : <Loader active inline='centered' /> }    
-                        
-                    </List>
-                </Card.Content>
-            </Card>
+            <div>
+                <Card fluid>
+                    <Card.Content header='Classes' />
+                    <Card.Content>
+                        <List divided verticalAlign='middle' selection animated size='large'>
+                            {this.props.sections.length != 0 ?
+                                this.state.sections.map((section) => 
+                                    <List.Item key={section._id} onClick={this.setState({activeSection: section})}>
+                                        <List.Content floated='right'>
+                                            <Button icon labelPosition='left'>
+                                                <Icon name='send outline' />
+                                                Message
+                                            </Button>
+                                        </List.Content>    
+                                        <List.Content floated='right' verticalAlign='middle'> 
+                                            <List.Header verticalAlign='middle'>{section.code}</List.Header>
+                                        </List.Content>
+                                        <List.Content verticalAlign='middle'>
+                                            <List.Header verticalAlign='middle'>{section.subject} - {section.sectionName}</List.Header>
+                                        </List.Content>
+                                    </List.Item>
+                                ) : <Loader active inline='centered' /> }
+                            
+                        </List>
+                    </Card.Content>
+                </Card>
+                {this.state.activeSection ? 
+                    <Card fluid>
+                        <Card.Content header='Classes' />
+                        <Card.Content>
+                            {this.state.activeSection.studentList.length != 0 ?
+                            <List divided verticalAlign='middle' selection animated size='large'>
+                                
+                                    this.state.activeSection.studentList.map((student, index) =>
+                                        <List.Item key={index}>
+                                            <List.Content floated='right'>
+                                                <Button icon labelPosition='left'>
+                                                    <Icon name='x' />
+                                                    Remove
+                                                </Button>
+                                            </List.Content>
+                                            <List.Content verticalAlign='middle'>
+                                                <List.Header verticalAlign='middle'>{student}</List.Header>
+                                            </List.Content>
+                                        </List.Item>
+                                    )
+                            </List> : "There are no students in the class."}
+                        </Card.Content>
+                    </Card> : null}
+            </div>    
         )
-
     }
 }
 
