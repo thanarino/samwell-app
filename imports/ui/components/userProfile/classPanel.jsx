6cacda0a520f5b2b5d70afba528e968c3076e3ab
Jonathan Arino <arinojonathan@gmail.com>
2018-04-30 03:07:29 +0800

    ui improvements

--- imports/ui/components/userProfile/classPanel.jsx
+++ imports/ui/components/userProfile/classPanel.jsx
@@ -99,7 +99,7 @@ class ClassPanel extends Component {
                                     {this.state.activeSection.studentList.length != 0 ?
                                         <List divided verticalAlign='middle' selection animated size='large'>
                                             {this.state.activeSection.studentList.map((student, index) =>{
-                                                let studentFound = _.filter(students, { '_id': student })[0];
+                                                let studentFound = _.filter(this.state.students, { '_id': student })[0];
                                                 return <List.Item key={index}>
                                                     <List.Content floated='right'>
                                                         <Button icon labelPosition='left'>
