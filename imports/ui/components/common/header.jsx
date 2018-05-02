3e504eac18e4c1509a445b4a8754fa034d9e600f
Jonathan Arino <arinojonathan@gmail.com>
2018-05-02 15:21:52 +0800

    add available indicator in header

--- imports/ui/components/common/header.jsx
+++ imports/ui/components/common/header.jsx
@@ -89,7 +89,9 @@ export default class SiteHeader extends Component {
                                     onClick={this.handleUserClick}
                                 >
                                     <Image avatar src={teacher.services.google ? teacher.services.google.picture : default_pp} />
-                                    {teacher.given_name}
+                                    
+                                    {`${teacher.given_name}  `}
+                                    {teacher.available ? <Label circular color={'green'} empty /> : <Label circular color={'red'} empty />}
                                 </Menu.Item>
                             </Menu.Menu>    
                             : <Menu.Menu position='right'>
