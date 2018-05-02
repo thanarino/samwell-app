615a145064127983c1ec2cdbcf06118473a65198
Jonathan Arino <arinojonathan@gmail.com>
2018-05-03 02:52:58 +0800

    add textbox to modal

--- imports/ui/components/userProfile/messageModal.jsx
+++ imports/ui/components/userProfile/messageModal.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal, Header, Icon } from 'semantic-ui-react';
+import { Button, Modal, Header, Icon, TextArea } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 
 export default class MessageModal extends Component {
