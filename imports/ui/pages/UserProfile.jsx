1f570622ae5ce69d2db20c072b342ad4369b823c
Jonathan Arino <arinojonathan@gmail.com>
2018-05-02 14:54:21 +0800

    add online checkbox

--- imports/ui/pages/UserProfile.jsx
+++ imports/ui/pages/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Label, Header, Icon, Button, Image, Card, Table } from 'semantic-ui-react';
+import { Grid, Label, Header, Icon, Button, Image, Card, Table, Checkbox } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import moment from 'moment';
