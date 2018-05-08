44dafb256e751c3bbf377d53cb320f0c74f19e86
Jonathan Arino <arinojonathan@gmail.com>
2018-05-08 21:08:09 +0800

    fix bogs

--- imports/ui/pages/UserProfile.jsx
+++ imports/ui/pages/UserProfile.jsx
@@ -99,9 +99,11 @@ ApprovedProfile = (props) => {
 
 toggle = (teacher, component) => {
     component.setState({ loading: true }, () => {
-        Meteor.call('teacher.setAvailable', teacher._id, !teacher.available);
-        console.log('teacher: ', teacher);
-        component.setState({ teacher, loading: false, pastValue: teacher.available });
+        const result = Meteor.call('teacher.setAvailable', teacher._id, !teacher.available);
+        if (result) {
+            console.log('teacher: ', teacher);
+            component.setState({ teacher, loading: false, pastValue: teacher.available });
+        }
     });
 }
 
